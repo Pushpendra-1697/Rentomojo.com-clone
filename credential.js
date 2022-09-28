6742b9b3cc765a55ce0f2567b737eabe77879663
Satyamjha24 <pananrocks18@gmail.com>
2022-09-28 23:48:13 +0530

    day2

---
+++ credential.js
@@ -0,0 +1,19 @@
+    
+   let  wfh=[
+    {img:'https://p.rmjo.in/productSquare/hdnqmf56-500x500.jpg',name:'Stuart Study Table',price:'149/mo'},
+    {img:'https://p.rmjo.in/productSquare/g4ow6lcd-500x500.jpg',name:'Morris Office Chair',price:'199/mo'},
+    {img:'https://p.rmjo.in/productSquare/jsxcydcf-500x500.jpg',name:'Auto Inclination Treadmill T-501',price:'2119/mo'},
+    {img:'https://p.rmjo.in/productSquare/ffg0b4na-500x500.jpg',name:'Cross Trainer C-500',price:'1109/mo'},
+    {img:'https://p.rmjo.in/productSquare/2hip7lup-500x500.jpg',name:'Exercise Cycle AB-100',price:'409/mo'},
+    {img:'https://p.rmjo.in/productSquare/mf3434ld-500x500.jpg',name:'Felix 3-Seater Fabric Sofa (Blue)',price:'789/mo'},
+    {img:'https://p.rmjo.in/productSquare/qcr7se17-500x500.jpg',name:'Barney Leather Recliner (Brown)',price:'799/mo'},
+    {img:'https://p.rmjo.in/productSquare/m7bwzi92-500x500.jpg',name:'Miller Office Chair',price:'149/mo'},
+    {img:'https://p.rmjo.in/productSquare/d25jc9wi-500x500.jpg',name:'Poise Study Table',price:'229/mo'},
+    {img:'https://p.rmjo.in/productSquare/n1hw74gw-500x500.jpg',name:'Dell Latitude , Intel Core...',price:'1839/mo'},
+    {img:'https://p.rmjo.in/productSquare/4o9rpi0p-500x500.jpg',name:'Dell XPS 13 , Intel Core...',price:'3679/mo'},
+    {img:'https://p.rmjo.in/productSquare/cfa7meld-500x500.jpg',name:'Lenovo Ideapad 330 , Int...',price:'1039/mo'},
+
+
+]
+
+export {wfh}
