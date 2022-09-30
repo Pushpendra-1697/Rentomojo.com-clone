ce566c48e9d7534175e0f41fbd47b8e91cded823
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2022-09-30 10:45:08 +0530

    added

---
+++ fw20_0536/packages_data.js
@@ -0,0 +1,78 @@
+let packages_data=[
+    {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/bedroom-v1_new.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/bedroom.svg",
+    room:"Bedroom",
+    contity:"20 Package(s)",
+    id:1
+  },
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/living-room-v2.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/living-room.svg",
+    room:"Living Room",
+    contity:"13 Package(s)",
+    id:2
+  },
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/appliances.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/study-room.svg",
+    room:"Appliances",
+    contity:"19 Package(s)",
+    id:3
+  },
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/study-room-v1.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/study-room.svg",
+    room:"Work From Home(WHF)",
+    contity:"6 Package(s)",
+    id:4
+  },
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/dining-v1.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/dining.svg",
+    room:"Kitchen & Dining",
+    contity:"2 Package(s)",
+    id:5
+  },
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/smart-packages.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/study-room.svg",
+    room:"Smart Home",
+    contity:"3 Package(s)",
+    id:6
+  },
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/fitness-packages.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/fitness-packages.svg",
+    room:"Fitness & Exercise",
+    contity:"1 Package(s)",
+    id:7
+  }
+]
+
+let apartment_data = [
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/studio-apartment.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/studio-apartment.svg",
+    room:"Studio Apartment",
+    contity:"1 Package(s)",
+    id:1
+  },
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/1-bhk-new.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/1-bhk.svg",
+    room:"1 BHK",
+    contity:"1 Package(s)",
+    id:2
+  },
+  {
+    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/2-bhk-new.jpg",
+    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/2-bhk.svg",
+    room:"2 BHK",
+    contity:"1 Package(s)",
+    id:3
+
+  }
+]
+
+export {packages_data,apartment_data};
