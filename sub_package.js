3dc4b4ecb0b28b1ac4ce0fa726cafb4c240e573e
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2022-10-01 23:13:32 +0530

    added

--- sub_package.js
+++ sub_package.js
@@ -3,34 +3,49 @@ import nvbr from './components/nvbr.js';
 document.querySelector('#navbar').innerHTML=nvbr();
 import auth from './components/auth.js';
     auth();
-
-
+ 
 let renderDom = () =>{
     let cont = document.getElementById("sidebar2");
     cont.innerHTML=null;
 
-    bedroom_data.forEach(({imageurl,title,price,id,value}) => {
+    bedroom_data.forEach((el) => {
 
         let div = document.createElement("div");
         div.setAttribute("class","main_div");
 
+        let div1 = document.createElement("div");
+        div1.setAttribute("class","fis_div");
+
+        let div2 = document.createElement("div");
+        div2.setAttribute("class","sec_div");
+
+        let div3 = document.createElement("div");
+        div3.setAttribute("class","the_div");
+
         let img = document.createElement("img");
-        img.src=imageurl;
+        img.src=el.img;
 
         let titl = document.createElement("h3");
-        titl.innerText=title;
+        titl.innerText= el.name;
 
         let pr=document.createElement("p");
-        pr.innerText=price
+        pr.innerText=el.price
 
-        div.append(img,title,price);
+        let add_btn = document.createElement("button");
+        add_btn.setAttribute("class","btn")
+        add_btn.innerText="Add to Cart";
+        add_btn.onclick=()=>{
+            addcart(el);
+
+        }
+        div1.append(img);
+        div2.append(titl);
+        div3.append(pr,add_btn);
+        div.append(div1,div2,div3);
         cont.append(div);
-
-
-
-    })
-
+    });
 };
+renderDom();
 
 let appendDom = () =>{
     let container = document.getElementById("containt");
@@ -58,25 +73,25 @@ let appendDom = () =>{
 let show = () => {
     let container1 =document.getElementById("container1");
     container1.innerHTML=null;
-
     typeroom.forEach(({imageurl_two,room,contity})=>{
-
         let div = document.createElement("div");
         div.setAttribute("class","first_div");
         let img = document.createElement("img");
         img.src= imageurl_two;
-
         let p = document.createElement("p");
         p.innerText=room;
-
         let c = document.createElement("p");
         c.innerText=contity;
-
         div.append(img,p,c);
         container1.append(div);
-
     })
-}
+};
 appendDom();
 show();
-//renderDom();
+
+let data =JSON.parse(localStorage.getItem('cart')) ||[];
+let addcart = (el) =>{
+    data.push(el)
+    alert(`Product Added Successfully`);
+    localStorage.setItem('cart',JSON.stringify(data));
+};
--- sub_packages_data.js
+++ sub_packages_data.js
@@ -1,92 +1,118 @@
-let bedroom_data=[
+let bedroom_data = [
     {
-        imageurl:"https://p.rmjo.in/moodShot/i05w1kj4-1024x512.jpg",
-        title:"Double Bed with Fridge & Washing Machine",
-        price:"₹1763/mo",
-        id:1,
-        value:"bedroom"
+        img: "https://p.rmjo.in/moodShot/i05w1kj4-1024x512.jpg",
+        name: "Double Bed with Fridge & Washing Machine",
+        price: 1763,
+        id: 1,
+        value: "bedroom",
     },
     {
-        imageurl:"https://p.rmjo.in/moodShot/y9knx5fz-1024x512.jpg",
-        title:"Magnum Bedroom Storage",
-        price:"₹947/mo",
-        id:2,
-        value:"bedroom"
+        img: "https://p.rmjo.in/moodShot/y9knx5fz-1024x512.jpg",
+        name: "Magnum Bedroom Storage",
+        price: 947,
+        id: 2,
+        value: "bedroom",
     },
     {
-        imageurl:"https://p.rmjo.in/moodShot/83x0ypkd-1024x512.jpg",
-        title:"Bedroom Basics",
-        price:"₹590/mo",
-        id:3,
-        value:"bedroom"
+        img: "https://p.rmjo.in/moodShot/83x0ypkd-1024x512.jpg",
+        name: "Bedroom Basics",
+        price: 590,
+        id: 3,
+        value: "bedroom",
     },
     {
-        imageurl:"https://p.rmjo.in/moodShot/pngfhl2b-1024x512.jpg",
-        title:"Magnum Queen Master Bedroom",
-        price:"₹987/mo",
-        id:4,
-        value:"bedroom"
+        img: "https://p.rmjo.in/moodShot/pngfhl2b-1024x512.jpg",
+        name: "Magnum Queen Master Bedroom",
+        price: 987,
+        id: 4,
+        value: "bedroom",
     },
     {
-        imageurl:"https://p.rmjo.in/moodShot/13ckfjbq-1024x512.jpg",
-        title:"Magnum Queen Basic Bedroom",
-        price:"₹449/mo",
-        id:5,
-        value:"bedroom"
+        img: "https://p.rmjo.in/moodShot/13ckfjbq-1024x512.jpg",
+        name: "Magnum Queen Basic Bedroom",
+        price: 449,
+        id: 5,
+        value: "bedroom",
     },
-]
+    {
+        img: "https://p.rmjo.in/moodShot/lcc7ot7j-1024x512.jpg",
+        name: "Hutch 2-Door Wardrobe & Saddle Shoe Rack",
+        price: 589,
+        id: 5,
+        value: "applince",
+    },
+    {
+        img: "https://p.rmjo.in/moodShot/j70gh422-1024x512.jpg",
+        name: "TV & Washing Machine Combo",
+        price: 1559,
+        id: 5,
+        value: "applince",
+    },
+    {
+        img: "https://p.rmjo.in/moodShot/3ca9d8c6-1024x512.jpg",
+        name: "Living Room Basics",
+        price: 541,
+        id: 5,
+        value: "applince",
+    },
+    {
+        img: "https://p.rmjo.in/moodShot/o235d7of-1024x512.jpg",
+        name: "Heathcliff Living Room",
+        price: 258,
+        id: 5,
+        value: "applince",
+    },
+
+];
 
 let roomData = [
     {
-       image:"https://www.rentomojo.com/public/images/category/icons/bedroom.svg",
-       type:"Bedroom",
-       cont:"20 Packages",
-       value:"bedroom" 
+        image: "https://www.rentomojo.com/public/images/category/icons/bedroom.svg",
+        type: "Bedroom",
+        cont: "20 Packages",
+        value: "bedroom",
     },
     {
-        image:"https://www.rentomojo.com/public/images/category/icons/living-room.svg",
-        type:"Living Room",
-        cont:"13 Packages",
-        value:"living room" 
-     },
-     {
-        image:"https://www.rentomojo.com/public/images/category/icons/fitness-packages.svg",
-        type:"Applince",
-        cont:"19 Packages",
-        value:"applince" 
-     },
-     {
-        image:"https://www.rentomojo.com/public/images/category/icons/study-room.svg",
-        type:"Work From Home",
-        cont:"6 Packages",
-        value:"work" 
-     },
-     
-]
+        image: "https://www.rentomojo.com/public/images/category/icons/living-room.svg",
+        type: "Living Room",
+        cont: "13 Packages",
+        value: "living room",
+    },
+    {
+        image: "https://www.rentomojo.com/public/images/category/icons/fitness-packages.svg",
+        type: "Applince",
+        cont: "19 Packages",
+        value: "applince",
+    },
+    {
+        image: "https://www.rentomojo.com/public/images/category/icons/study-room.svg",
+        type: "Work From Home",
+        cont: "6 Packages",
+        value: "work",
+    },
+];
 
 
 let typeroom = [{
-    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/studio-apartment.jpg",
-    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/studio-apartment.svg",
-    room:"Studio Apartment",
-    contity:"1 Package(s)",
-    id:1
-  },
-  {
-    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/1-bhk-new.jpg",
-    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/1-bhk.svg",
-    room:"1 BHK",
-    contity:"1 Package(s)",
-    id:2
-  },
-  {
-    imageurl_one:"https://www.rentomojo.com/public/images/category/package-bg/2-bhk-new.jpg",
-    imageurl_two:"https://www.rentomojo.com/public/images/category/icons/2-bhk.svg",
-    room:"2 BHK",
-    contity:"1 Package(s)",
-    id:3
+    imageurl_one: "https://www.rentomojo.com/public/images/category/package-bg/studio-apartment.jpg",
+    imageurl_two: "https://www.rentomojo.com/public/images/category/icons/studio-apartment.svg",
+    room: "Studio Apartment",
+    contity: "1 Package(s)",
+    id: 1,
+},
+{
+    imageurl_one: "https://www.rentomojo.com/public/images/category/package-bg/1-bhk-new.jpg",
+    imageurl_two: "https://www.rentomojo.com/public/images/category/icons/1-bhk.svg",
+    room: "1 BHK",
+    contity: "1 Package(s)",
+    id: 2,
+},
+{
+    imageurl_one: "https://www.rentomojo.com/public/images/category/package-bg/2-bhk-new.jpg",
+    imageurl_two: "https://www.rentomojo.com/public/images/category/icons/2-bhk.svg",
+    room: "2 BHK",
+    contity: "1 Package(s)",
+    id: 3,
+}];
 
-  }]
-
-
-export {bedroom_data,roomData,typeroom};
+export { bedroom_data, roomData, typeroom };
